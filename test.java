63072164a96cf4a75fd25ee8c2cc7aae48785d21
fareastern <36926187+fareastern@users.noreply.github.com>
2025-02-23 16:06:00 +0300

    Add files via upload
    Домашнее задание модуля "Введение в Java" (2 задания)

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main (String[] args){
+        // task 1
+        System.out.println("Vladislav Udovenko");
+
+        // task 2
+        int income = 100000;
+        int spending = 55000;
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
